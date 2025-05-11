8518247cf55adc9047ce8ae6ce7ce8883ecd2b42
hassanahmed987 <hassanahmedsiddqui@gmail.com>
2025-05-11 20:39:57 +0500

    Update pieces.h

--- pieces.h
+++ pieces.h
@@ -15,48 +15,50 @@ public:
     bool hasMoved;
 
     Piece(PieceType t, Color c, const sf::Texture& texture);
-
     virtual ~Piece() {}
 
-    virtual bool isValidMove(int sx, int sy, int dx, int dy, Piece* board[8][8]) = 0;
+    // Marked const pointer for compatibility with const Game methods
+    virtual bool isValidMove(int sx, int sy, int dx, int dy, Piece* const board[8][8]) = 0;
 
     static std::string pieceToString(PieceType t, Color c);
 };
 
+// ----- Subclasses -----
+
 class King : public Piece {
 public:
     King(Color c, const sf::Texture& tex);
-    bool isValidMove(int sx, int sy, int dx, int dy, Piece* board[8][8]) override;
+   bool isValidMove(int sx, int sy, int dx, int dy, Piece* const board[8][8]) override;
 };
 
 class Queen : public Piece {
 public:
     Queen(Color c, const sf::Texture& tex);
-    bool isValidMove(int sx, int sy, int dx, int dy, Piece* board[8][8]) override;
+    bool isValidMove(int sx, int sy, int dx, int dy, Piece* const board[8][8]) override;
 };
 
 class Rook : public Piece {
 public:
     Rook(Color c, const sf::Texture& tex);
-    bool isValidMove(int sx, int sy, int dx, int dy, Piece* board[8][8]) override;
+    bool isValidMove(int sx, int sy, int dx, int dy, Piece* const board[8][8]) override;
 };
 
 class Bishop : public Piece {
 public:
     Bishop(Color c, const sf::Texture& tex);
-    bool isValidMove(int sx, int sy, int dx, int dy, Piece* board[8][8]) override;
+    bool isValidMove(int sx, int sy, int dx, int dy, Piece* const board[8][8]) override;
 };
 
 class Knight : public Piece {
 public:
     Knight(Color c, const sf::Texture& tex);
-    bool isValidMove(int sx, int sy, int dx, int dy, Piece* board[8][8]) override;
+    bool isValidMove(int sx, int sy, int dx, int dy, Piece* const board[8][8]) override;
 };
 
 class Pawn : public Piece {
 public:
     Pawn(Color c, const sf::Texture& tex);
-    bool isValidMove(int sx, int sy, int dx, int dy, Piece* board[8][8]) override;
+    bool isValidMove(int sx, int sy, int dx, int dy, Piece* const board[8][8]) override;
 };
 
 #endif
